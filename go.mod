6ee4b8a835a5e96ce4867d1db55bd5c690f8cd7e
xincao9 <xincao9@gmail.com>
2025-02-07 15:13:19 +0800

    OK

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module kbs-crawler
+module lei-crawler
 
 go 1.23.4
 
--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"strings"
 
 	"github.com/playwright-community/playwright-go"
 )
 
 var (
-	pw      *playwright.Playwright
-	browser playwright.Browser
-	page    playwright.Page
+	pw *playwright.Playwright
 )
 
 func init() {
@@ -19,14 +23,51 @@ func init() {
 	if err != nil {
 		log.Fatalf("Open playwright error: %v", err)
 	}
-	browser, err = pw.Chromium.Launch()
+}
+
+func NewBrowser() (browser playwright.Browser, page playwright.Page, err error) {
+	ips := dynamicIP()
+	if ips == nil || len(ips) <= 0 {
+		err = errors.New("dynamic ip acquisition failed")
+		return
+	}
+	ip := ips[0]
+	browser, err = pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+		Proxy: &playwright.Proxy{
+			Server: fmt.Sprintf("http://%s", ip),
+		},
+	})
 	if err != nil {
-		log.Fatalf("Open browser error: %v", err)
+		return
 	}
 	page, err = browser.NewPage()
 	if err != nil {
-		log.Fatalf("Open page error: %v", err)
+		return
 	}
+	return
+}
+
+type DynamicIP struct {
+	Code int      `json:"code"`
+	Data []string `json:"data"`
+	Msg  string   `json:"msg"`
+}
+
+func dynamicIP() []string {
+	resp, err := http.Get("http://list.sky-ip.net/user_get_ip_list?token=ywtPFyCzEaKqVjKG1666769437853&type=datacenter&qty=1&country=&time=5&format=json&protocol=http")
+	if err != nil {
+		return nil
+	}
+	defer resp.Body.Close()
+	buf := bytes.Buffer{}
+	io.Copy(&buf, resp.Body)
+	log.Printf("dynamicIP resp: %s", buf.String())
+	var dip DynamicIP
+	err = json.Unmarshal(buf.Bytes(), &dip)
+	if err != nil {
+		return nil
+	}
+	return dip.Data
 }
 
 type Article struct {
@@ -35,10 +76,26 @@ type Article struct {
 		PublishTime string `json:"publish-time"`
 	} `json:"info"`
 	Content string `json:"content"`
+	Img     string `json:"img"`
 }
 
 func main() {
-	uris, err := uris("https://www.leisu.com/news/catalog-zuqiu")
+	browser, page, err := NewBrowser()
+	defer func() {
+		if browser != nil {
+			err = browser.Close()
+			log.Printf("browser.Close err: %v\n", err)
+		}
+		if pw != nil {
+			err = pw.Stop()
+			log.Printf("pw.Stop err: %v\n", err)
+		}
+	}()
+	if err != nil {
+		log.Printf("NewBrowser err: %v\n", err)
+		return
+	}
+	uris, err := uris(page, "https://www.leisu.com/news/catalog-zuqiu")
 	if err != nil {
 		log.Printf("error: %v\n", err)
 		return
@@ -51,18 +108,16 @@ func main() {
 		var article Article
 		article.Title, _ = page.Locator(".article-detail .title").TextContent()
 		article.Info.PublishTime, _ = page.Locator(".article-detail .article-info .publish-time").TextContent()
-		strict := true
-		article.Content, err = page.InnerHTML(".article-detail .article-content", playwright.PageInnerHTMLOptions{
-			Strict: &strict,
-		})
-		if article.Content != "" {
+		article.Content, _ = page.Locator(".article-detail .article-content").TextContent()
+		article.Img, _ = page.Locator(".article-detail img").GetAttribute("src")
+		if strings.TrimSpace(article.Content) != "" {
 			raw, _ := json.Marshal(article)
 			log.Printf("article: %s\n", raw)
 		}
 	}
 }
 
-func uris(p string) ([]string, error) {
+func uris(page playwright.Page, p string) ([]string, error) {
 	if _, err := page.Goto(p); err != nil {
 		return nil, err
 	}
